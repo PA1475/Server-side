73e19b43ac70a9b1b6cfd72620ef858954dd6fb2
William Lebrato <william.lebrato@gmail.com>
2022-04-22 17:05:50 +0200

    To laptop

--- Dashboard/Sensors/PrimeTime.py
+++ Dashboard/Sensors/PrimeTime.py
@@ -6,15 +6,28 @@ from datetime import date, datetime
 import time
 import numpy as np
 
+
 class Timeline:
     def __init__(self,increment = 600,commit = 1649245858):
         self.commit = commit
-        self.now = int(time.time())
+        self.now = time.time()
         self.increment = increment
+        self.og_colors = ["grey","red","yellow","green","blue"]
+        self.colors = ["grey","red","yellow","green","blue"]
+
 
         self.df = self.get_df()
         self.timeline = self.create_timeline()
-    
+
+    def change_color(self,color):
+        if self.colors[self.og_colors.index(color)] == self.og_colors[self.og_colors.index(color)]:
+            self.color[self.og_colors.index(color)] = "gray"
+        else:
+            self.color[self.og_colors.index(color)] = color
+
+    def restart(self):
+        self.timeline = self.create_timeline()
+
     def get_df(self):
         df = pd.read_csv(Path(__file__).parent/"emotions.csv")
         df = df[df["timestamps"].between(self.now,self.commit)]
@@ -26,18 +39,25 @@ class Timeline:
         if segment.shape[0] == 0:
             dominant = 0
         else:
-            dominant = (segment["mood"].mode())
-        return dominant
+            dominant = (segment["mood"].mode())[0]
+        return self.colors[dominant]
 
-    def create_timeline(self):
+    def create_timeline(self,now = None):
         timeline = pd.DataFrame(columns=["timestamps","mood"])
+        self.now = int(time.time())
 
-        start = self.commit - self.commit%86400
+
         end = self.now + (86400-self.now%86400)
+        start = self.commit - self.commit%86400
+        if end-start < 86400*8:
+            start = end - 86400*8
+        """
+        IF statement makes sure the timeline always is 7 days long
+        """
+
 
         days = int((end-start)/86400)
         for i in range(days*int(86400/self.increment)):
-            print(i)
             details = {"timestamps":[start],"mood":[self.get_dominant(start)]}
             segment = pd.DataFrame(details)
             timeline = pd.concat([timeline,segment],ignore_index=True, sort=False)
@@ -50,10 +70,13 @@ class Timeline:
         df = self.timeline[self.timeline["timestamps"].between(start,end)]
         return df
 
-    def figure(self,start):
+    def day(self,start):
         df = self.get_segment(start)
-        fig = px.bar(df,x="timestamps",y="mood",color ="mood",height=200)
-        fig.show()
-
+        fig = px.histogram(df,x="timestamps",color = "mood",nbins = int(86400/self.increment))
+        return fig
     
-timeline = Timeline()
+    def figure(self,start):
+        plots = []
+        for i in range(7):
+            plots.append(self.day(start))
+            start -= 86400
